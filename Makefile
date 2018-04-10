8dca01909147919f6ea65a7a90a5d4aa7261a88d
Patric Carman <patric.carman@gmail.com>
2018-04-10 18:21:32 -0400

    Add some shitty broken code

--- Makefile
+++ Makefile
@@ -1,12 +1,9 @@
 .PHONY : clean debug classroom
+# we're running the interpreter, not compiler
 all:
-	gch assign4 src/assign4.hs
+	ghci -i src/assign4.hs
 
-# delete non-source files
-clean:
-	rm -f assign4
-	rm -f tests/output*.txt
 
 # push to classroom. $USER is your cs username
 classroom:
-	scp -r * $(USER)@classroom.cs.unc.edu:/afs/cs.unc.edu/proj/courses/comp524-s18/students/$(USER)/ASSIGN3
+	scp -r * $(USER)@classroom.cs.unc.edu:/afs/cs.unc.edu/proj/courses/comp524-s18/students/$(USER)/ASSIGN4
--- src/assign4.hs
+++ src/assign4.hs
@@ -1,6 +1,31 @@
 import System.Environment
+import Data.List
+import Data.Char
+
+-- this works to see if foo is an element of the permutations of "oof":
+-- lowerCase "foo" `elem` permutations "oof"
+
+-- so more generally we want this:
+-- lowerCase INPUTWORD `elem` permutations ALL_WORDS_IN_LIST
+
+-- n.b. no duplicates should be returned! e.g. meet and meet should be treated as the same word.
 
 main = do
-    [dictFile, word] <- getArgs
-    dict <- readFile dictFile
-    -- idk
+   args <- getArgs
+   content <- readFile (args !! 0)
+   fileWords <- lines content
+   map lowerCase fileWords
+   -- let bar = map lowerCase fileWords
+   -- return bar
+
+-- takes a string and returns a lowercase versoin of it
+-- lowercase "FOO" -> "foo"
+lowerCase :: String -> String
+lowerCase [] = []
+lowerCase (start:end) = [toLower start] ++ lowerCase end
+--
+shorterThan :: String -> Int -> IO [String]
+shorterThan fileName len = do fileContents <- readFile fileName
+                              let fileWords = lines fileContents
+                              let shorterThanWords = [word | word <- fileWords, (length word) < len]
+                              return shorterThanWords
