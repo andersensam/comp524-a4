9006174f2f6ae6a1d88df13153fb5d74a790c363
Patric Carman <patric.carman@gmail.com>
2018-04-11 13:05:03 -0400

    cleanup

--- src/assign4.hs
+++ src/assign4.hs
@@ -16,30 +16,21 @@ import Data.Char
 -- n.b. no duplicates should be returned! e.g. meet and meet should be treated as the same word.
 
 assign4 :: String -> String -> IO [String]
-string dict word = do
+String dict word = do
 	fileContents <- readFile dict
-	fileLines <- lines dict
+	fileLines <- lines fileContents
 
-
-main = do
-   args <- getArgs
-   content <- readFile (args !! 0)
---    fileWords <- lines content
---    map lowerCase fileWords
---    -- let bar = map lowerCase fileWords
---    -- return bar
-
--- Takes a string and returns a lowercase versoin of it
+-- Takes a String and returns a lowercase versoin of it
 -- e.g. lowercase "FOO" -> "foo"
--- @param string array
+-- @param String
 lowerCase :: String -> String
 lowerCase [] = []
 lowerCase (start:end) = [toLower start] ++ lowerCase end
 
 
 -- Dong said we need this method but I don't know why...
--- @param fileName string the path to the dictionary file
--- @param len int words should be shorter than this length
+-- @param fileName String the path to the dictionary file
+-- @param len Int words should be shorter than this length
 shorterThan :: String -> Int -> IO [String]
 shorterThan fileName len = do
 	fileContents <- readFile fileName
@@ -50,8 +41,8 @@ shorterThan fileName len = do
 -- if permutations(inputWord) = dictionaryWord, then return dictionaryWord
 -- e.g. isPermutation("foo","bar") = null
 -- e.g. isPermutation("foo","oof") = "oof"
--- @param inputWord string a word inputted by user
--- @param dictionaryWord string a word in the dictionary
+-- @param inputWord String a word inputted by user
+-- @param dictionaryWord String a word in the dictionary
 -- @return dictionaryWord String if inputWord is a permutation of dictionaryWord. Else, null
 -- TODO this doesn't work
 isPermutation :: String -> String -> Maybe String
